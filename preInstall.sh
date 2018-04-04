49657be509f68842dfdc850e90e445b83580aaf2
Frank Magazu <FMAGAZU@MITRE.ORG>
2018-04-04 17:49:09 -0400

    added docker compose

--- preInstall.sh
+++ preInstall.sh
@@ -15,9 +15,6 @@ fi
  apt-get -y install libopenal1 libopenal1:i386 lib32z1 lib32ncurses5 libbz2-1.0:i386 \
  					libstdc++6:i386 libxxf86vm1 libglu1:i386 libxrandr-dev libxrandr2:i386 zip unzip
 
-wget http://http.us.debian.org/debian/pool/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u7_i386.deb
- dpkg -i libssl1.0.0_1.0.1t-1+deb8u7_i386.deb
-
  dpkg -i gasPumpSimulator.deb
  sudo chmod +x /opt/gasPumpSimulator/gasPumpSimulator
- /opt/gasPumpSimulator/gasPumpSimulator
+ LD_PRELOAD=`pwd`/libssl.so.1.0.0:`pwd`/libcrypto.so.1.0.0 /opt/gasPumpSimulator/gasPumpSimulator
